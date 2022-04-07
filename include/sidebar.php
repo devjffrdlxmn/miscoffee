d4fd23882792d66a11c44edbf1b0b319ca03c422
Jeffrid Laxamana <jeffrid.laxamana@cvsu.edu.ph>
2022-04-07 13:44:26 +0800

    added admin template

---
+++ include/sidebar.php
@@ -0,0 +1,51 @@
+
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-light-primary elevation-1 " >
+    <!-- Brand Logo -->
+    <a href="index.php" class="brand-link "  >
+      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image  " >
+      <span class="brand-text font-weight-bolder ">
+        <h class="text-success">CVSU-CCAT</h> CAMPUS
+          
+        </span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+
+          <li class="nav-item">
+            <a href="index.php" class="nav-link">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>Dashboard</p>
+            </a>
+          </li>
+         
+
+          <li class="nav-item">
+            <a href="generatePDF.php" class="nav-link" target="_blank">
+              <i class="fa fa-list-alt nav-icon"></i>
+              <p>Voting Result Summary </p>
+            </a>
+          </li>
+          
+        
+          
+              
+  
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+</aside>
+  
+<script>
+   
+</script>
